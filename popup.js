802a0d69090eefd69f7aefe89d160dba935ef43c
Amunak <git@amunak.net>
2024-09-04 00:13:55 +0200

    Adds better styles and animation for clicking URL in the popup
    to indicate that it has been copied to clipboard.

--- popup.js
+++ popup.js
@@ -86,6 +86,10 @@ async function handleActiveTabs(tabs) {
 		linkEl.innerText = unsubLink
 		linkEl.addEventListener('click', () => {
 			navigator.clipboard.writeText(linkEl.innerText)
+			linkEl.classList.add('hl')
+			setTimeout(() => {
+				linkEl.classList.remove('hl')
+			}, 100)
 		})
 
 		linkContainerEl.innerText = browser.i18n.getMessage('unsubLink')
